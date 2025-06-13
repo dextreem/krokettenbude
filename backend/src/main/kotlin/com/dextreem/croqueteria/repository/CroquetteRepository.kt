ba5eb21637baa806e82729b4055f2551e87e7995
Dominic Buchheit <dominic@dobuch.de>
2025-06-13 20:47:08 +0200

    Remove unused reqpository function

--- backend/src/main/kotlin/com/dextreem/croqueteria/repository/CroquetteRepository.kt
+++ backend/src/main/kotlin/com/dextreem/croqueteria/repository/CroquetteRepository.kt
@@ -2,31 +2,9 @@ package com.dextreem.croqueteria.repository
 
 import com.dextreem.croqueteria.entity.Croquette
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor
-import org.springframework.data.jpa.repository.Query
 import org.springframework.data.repository.CrudRepository
 import org.springframework.stereotype.Repository
 
 @Repository
 interface CroquetteRepository : CrudRepository<Croquette, Int>, JpaSpecificationExecutor<Croquette> {
-    @Query("""
-        SELECT 
-            c.id AS id, 
-            c.name AS name, 
-            c.country AS country,
-            c.description AS description,
-            c.crunchiness AS crunchiness,
-            c.spiciness AS spiciness,
-            c.vegan AS vegan,
-            c.form AS form,
-            c.imageUrl AS imageUrl,
-            c.createdAt AS createdAt,
-            c.updatedAt AS updatedAt,
-            AVG(r.rating) AS averageRating 
-        FROM Croquette c 
-        LEFT JOIN Rating r ON c.id = r.croquette.id 
-        GROUP BY c.id, c.name, c.description, c.imageUrl
-    """)
-    fun findAllWithAverageRating(): List<Croquette>
-
-    fun findByCountry(country: String): List<Croquette>
 }
