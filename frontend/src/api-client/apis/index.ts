cb340f4d0b94c16d68f188908abf2f56d42f85bc
Dominic Buchheit <dominic@dobuch.de>
2025-06-17 16:43:31 +0200

    Add openapi generated API code

---
+++ frontend/src/api-client/apis/index.ts
@@ -0,0 +1,7 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './CommentRestAPIEndpointsApi';
+export * from './CroquetteRestAPIEndpointsApi';
+export * from './RatingRestAPIEndpointsApi';
+export * from './RecommendationControllerApi';
+export * from './UserRestAPIEndpointsApi';
---
+++ frontend/src/api-client/models/index.ts
@@ -0,0 +1,19 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './CommentCreateRequest';
+export * from './CommentResponse';
+export * from './CommentUpdateRequest';
+export * from './CroquetteCreateRequest';
+export * from './CroquetteLLMRecommendationRequest';
+export * from './CroquetteRecommendationRequest';
+export * from './CroquetteRecommendationResponse';
+export * from './CroquetteResponse';
+export * from './CroquetteUpdateRequest';
+export * from './LoginRequest';
+export * from './LoginResponse';
+export * from './RatingCreateRequest';
+export * from './RatingResponse';
+export * from './RatingUpdateRequest';
+export * from './UserCreateRequest';
+export * from './UserResponse';
+export * from './UserUpdateRequest';
